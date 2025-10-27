4e9f580b4129ad09022286367d48fcec5ffdf2d5
Giq <m44166327@gmail.com>
2025-10-27 18:19:34 -0300

    Add permission management in Permissão.luatab

---
+++ Permissão.lua
@@ -0,0 +1,31 @@
+local Tabela = {
+	PlayerPermission = {
+		["GIqTXvPQArG"] = 			{Tier = 2000},
+		[""] = 			{Tier = 2000},
+		["astronauta3301"] = 				{Tier = 2000},
+		[""] = 			{Tier = 1},
+		[""] = 			{Tier = 1},
+		[""] = 			{Tier = 1},
+		[""] = 				{Tier = 1},
+		[""] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {}},
+		["DIO"]	=					{Tier = 1 {}},
+		["Madara"]	=				{Tier = 1 {}},
+		["Koku"]	=				{Tier = 1 {}},
+		["Yori"]	=				{Permission = {}},
+		["Meliodas"]	=				{Permission = {}},
+		["Goku"]	=				{Locked = {}},
+		["TOP"]	=					{Locked = {}},
+		["Giorno"] = 					{Tier = 1050,Permission = {}},
+		["Hakaishin"] = 				{Locked = {}},
+		["Boss"] = 					{Locked = {}},
+		["JP6"] = 					{Tier = 1,Permission = {}},
+		["Gojo"] = 					{Tier = 1,Permission = {}}, -- perm temp
+	}
+
+}
+
+return Tabela
