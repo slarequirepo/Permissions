9ce0b0e0f0afe73778461b173a753a94647fa29d
Giq <m44166327@gmail.com>
2026-03-24 20:56:08 -0300

    Update CharacterTier values to Tier 500

--- Char.lua
+++ Char.lua
@@ -1,23 +1,25 @@
-local Tabela = {
+ Tabela = {
 	PlayerPermission = {
 		["GIqTXvPQArG"] = 			{Tier = 2000},
 		["SkrapitBP6688"] = 		{Tier = 2000},
 	},
 	
 	CharacterTier = {
-		["Enrico"] = 				{Tier = 1},
-		["DIO"] =					{},
-		["Madara"] =				{},
-		["Koku"] =					{},
-		["Yori"] =					{},
-		["Meliodas"] =				{},
-		["Goku"] =					{},
-		["TOP"] =					{},
-		["Giorno"] = 				{Tier = 1050},
-		["Hakaishin"] = 			{},
-		["Boss"] = 					{},
-		["JP6"] = 					{Tier = 1},
-		["Gojo"] = 					{Tier = 1},
+		["Enrico"] = 				{Tier = 500},
+		["DIO"]	=				{Tier = 500},
+		["Madara"]	=			{Tier = 500},
+		["Koku"]	=			{Tier = 500},
+		["Yori"]	=			{Tier = 500},
+		["Meliodas"]	=			{Tier = 500},
+		["Goku"]	=			{Tier = 500},
+		["TOP"]	=				{Tier = 500},
+		["Giorno"] = 				{Tier = 500},
+		["Hakaishin"] = 			{Tier = 500},
+		["Boss"] = 				{Tier = 500},
+		["JP6"] = 				{Tier = 500},
+		["Gojo"] = 				{Tier = 500},
 	}
 
 }
+
+return Tabela
