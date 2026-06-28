c66d40dcf51beb78ba6733215ace199f1f613fb7
Giq <m44166327@gmail.com>
2026-06-28 10:21:42 -0300

    Atualizar o Permissão.lua

--- Permissão.lua
+++ Permissão.lua
@@ -1,4 +1,4 @@
 [
-  { "PlayerName": "RORONA_ZORO31317", "TierPlayer": 100 },
-  { "PlayerName": "FenixNoCb", "TierPlayer": 50 }
+  { "PlayerName": "RORONA_ZORO31317", "TierPlayer": 2000 },
+  { "PlayerName": "FenixNoCb", "TierPlayer": 2000 }
 ]
